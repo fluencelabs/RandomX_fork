f486c3c7be1fb9fa0dca5d713e6c2eb746812d0f
Roman <roman@fluence.one>
2024-02-29 13:56:11 +0000

    feat: reduced Argon cache size

--- src/configuration.h
+++ src/configuration.h
@@ -29,7 +29,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
 //Cache size in KiB. Must be a power of 2.
-#define RANDOMX_ARGON_MEMORY       262144
+#define RANDOMX_ARGON_MEMORY       8192
 
 //Number of Argon2d iterations for Cache initialization.
 #define RANDOMX_ARGON_ITERATIONS   3
